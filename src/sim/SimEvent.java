8c49ea8587de11d21ac9946f37a073ac6624f439
Emil Aasa <emilaasa@gmail.com>
2016-03-10 10:47:09 +0100

    JavaDoc for SimEvent

--- src/sim/SimEvent.java
+++ src/sim/SimEvent.java
@@ -1,15 +1,28 @@
 package sim;
 
 /**
- * Created by emilaasa on 09/03/16.
+ * A general Simulation Event
  */
 public interface SimEvent {
 
+    /**
+     * execute should :
+     * execute the state changes on the event and trigger the generation of additional events
+     */
     void execute();
 
+    /**
+     * @return a double representing the time of the event
+     */
     double getTime();
 
+    /**
+     * @return a boolean that indicates if an event should be removed or not
+     */
     boolean getRemove();
 
+    /**
+     * @return a boolean that STOPS the event loop
+     */
     boolean getSTOP();
 }
